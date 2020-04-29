bdf0e61955b4e0ae1775e40b208208afcd7d8dfb
Thomasr <thomas.ribeiro.ext@synchro.com.br>
2020-04-29 13:01:14 -0300

    add component to the tests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import ConvertTemp from './components/ConvertTemp'
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h2>Conversão de temperatura </h2>
+        <ConvertTemp></ConvertTemp>      
+      </>
+    );
+  }
 }
 
 export default App;
